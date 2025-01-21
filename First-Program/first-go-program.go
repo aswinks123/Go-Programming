50739f1fd58777eeadc2a1b0a7acbb91e6140aca
aswin <aswinks123>
2025-01-20 19:02:13 -0500

    First go program

---
+++ First-Program/first-go-program.go
@@ -0,0 +1,17 @@
+
+/*
+=============================================================================
+DEVELOPER: Aswin KS
+PURPOSE: First Go Program
+About: 
+DATE: 20-01-2025
+=============================================================================       
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Aswin!")
+}
