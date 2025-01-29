ef10d35f986974adf7234d6228bca1a4555c9042
Aswinks1995 <aswinks1995@outlook.com>
2025-01-29 14:34:46 -0500

    Added program for constant declerations

---
+++ variables_and_constants/constants/constants-declerations.go
@@ -0,0 +1,28 @@
+/*
+=============================================================================
+DEVELOPER: Aswin KS
+PURPOSE: To demonstrate different ways to declare Constants in go
+About: This program declare and use Constant values
+DATE: 29-01-2025
+=============================================================================
+*/
+
+package main //package main is used when you're writing an executable program (not a library).
+
+import "fmt"
+
+func main() {
+
+	// Create a constant
+	const pi float64 = 3.1415
+	const radis = 5
+	const name string = "aswin"
+	area := pi * radis * radis //Type of variable area is automatically found from the result of the output
+	fmt.Println("Area is:", area)
+
+	// Constants cannot be assigned another value once it is defines
+	fmt.Println("Name is:", name)
+	name = "Eldho" // This reassignment failed
+	fmt.Println("Area is:", name)
+
+}
