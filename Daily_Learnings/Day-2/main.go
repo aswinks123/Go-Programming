5fa9c7d8c63bfaa1678da0403a94939011bcca8a
Aswinks1995 <aswinks1995@outlook.com>
2025-02-24 15:44:13 -0500

    Day-2 program added

--- Daily_Learnings/Day-2/main.go
+++ Daily_Learnings/Day-2/main.go
@@ -16,29 +16,29 @@ package main
 
 import "fmt"
 
-//Function to find the large number in the slice
+//Function to find the largest number in the slice
 func largenumber(nums []int) (int, error) { //function that returns int and an error
 
-	if len(nums) == 0 { // checking if slice is empty or not. Retuen an error message if empty
+	if len(nums) == 0 { // checking if slice is empty or not. Return an error message if empty. 0 is returned because the function expects an int return value
 		return 0, fmt.Errorf("Empty slice. Please add value to slice first")
 	}
 
 	large := nums[0] //assume first value as large
 	for i := 1; i < len(nums); i++ {
 		if nums[i] > large { // checking each element after the first value to the "large" variable
-			large = nums[i] // idfthe calue is large then assign that value to the variable "large"
+			large = nums[i] // if the value is large, then assign that value to the variable "large"
 		}
 	}
-	return large, nil //return the large a,nd nil ( nil is returned because thge functrion expects an error return )
+	return large, nil //return the large and nil ( nil is returned because the function expects an error return )
 }
 
 func main() {
 	var nums = []int{3, 1, 35, 7, 9, 5}
 	var larger, err = largenumber(nums) // calling the function
-	if err != nil {                     //if err is not empty then print the error that was returned
+	if err != nil {                     //if variable err is not empty, then print the error that was returned
 		fmt.Print(err)
 		return // Exit the program
 	}
-	fmt.Println("Largest element is:", larger) // if not an empty slice then print the large element
+	fmt.Println("Largest element is:", larger) // if not an empty slice then print the largest element
 
 }
